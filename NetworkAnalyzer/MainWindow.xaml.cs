3f2ac76616f35e1e5d9927d23771cb48ce11a423
kodack64 <kodack64@msn.com>
2015-07-22 17:55:05 +0900

    first commit

---
+++ NetworkAnalyzer/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkAnalyzer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Callback_PlanStart(object sender, RoutedEventArgs arg)
+        {
+        }
+        private void Callback_PlanCancel(object sender, RoutedEventArgs arg)
+        {
+        }
+    }
+}
